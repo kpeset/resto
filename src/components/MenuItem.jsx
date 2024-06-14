479d3c1e5c36debd6f47a86a2f13259f000145e0
Kevin Peset <kevin.peset@gmail.com>
2024-06-14 08:52:55 +0200

    initial commit

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+function MenuItem(props) {
+  //create a state isFavorite that has the inital value of isFavorite that comes from the props
+
+  return (
+    <section className="itemContainer">
+      <figure className="imgContainer">
+        {/* the image will receive the url src from the props */}
+        <img src="" alt="" />
+        <figcaption>
+          {/* the h2 will receive the item name from the props */}
+          <h2>{}</h2>
+          {/* the p will receive the item description from the props */}
+          <p>{}</p>
+        </figcaption>
+      </figure>
+      {/* the span will receive the item price from the props */}
+      <span>{} EUR</span>
+
+      {/* the button to play with the isFavorite state:
+              - onClick, will toggle the isFavorite state,
+              - content will be conditionally rendered as "❤️" or "🖤", depending on the value of isFavorite
+          */}
+      <button>{}</button>
+    </section>
+  );
+}
+
+export default MenuItem;
---
+++ src/components/MenuList.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import MenuItem from "./MenuItem";
+
+function MenuList({ foodItems }) {
+  console.log(foodItems);
+  return (
+    <>{/*render a MenuItem component to each element of the props array*/}</>
+  );
+}
+
+export default MenuList;
